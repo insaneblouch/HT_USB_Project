0b08a47c06a26ad70b3707a47212d86737c956a7
insaneblouch <insaneblouch@gmail.com>
2015-05-04 23:20:56 +0800

    Success Build UART for fixed signal

--- HT66FB550_USB_Common/HT66FB550_UART.c
+++ HT66FB550_USB_Common/HT66FB550_UART.c
@@ -14,6 +14,8 @@
 
 #define tm3reload()	_tm3al=tm3al_reload;_tm3ah=tm3ah_reload;
 
+char uart_step,uart_pin_buf;
+
 void __attribute((interrupt(0x24)))	ISR_timer3(void)
 {
 	if(_t3af){
@@ -27,7 +29,35 @@ void __attribute((interrupt(0x24)))	ISR_timer3(void)
 
 void UART_ISR(void)
 {
-	UART_TX_PIN = !UART_TX_PIN;
+	UART_TX_PIN = uart_pin_buf;
+	switch(uart_step)
+	{
+		case 0:
+			uart_pin_buf=0;
+			break;	
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+			uart_pin_buf=1;
+			break;
+		case 2:
+		case 4:
+		case 6:
+		case 8:
+			uart_pin_buf=0;
+			break;
+		case 9:		//stop bit
+		case 10:	//stop bit hold
+		case 11:	//stop bit hold
+			uart_pin_buf=1;
+			break;
+		case 12:
+			uart_step = -1;
+			uart_pin_buf=1;
+			break;		
+	}
+	uart_step++;
 	return;
 }
 void UART_Init(void)
@@ -39,6 +69,8 @@ void UART_Init(void)
 
 void UART_Start(void)
 {
+	uart_step = 0;
+	uart_pin_buf = 1;
     _mf3e = 1;
     _t3ae = 1;
     _emi = 1;
